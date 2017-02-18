d58606b9475d8724068712ef8b5a243d48876044
opticyclic <opticyclic@gmail.com>
2017-02-18 14:05:48 -0500

    Implement getMkFromLine to pass the tests

--- src/main/java/com/github/opticyclic/mkparser/MkParser.java
+++ src/main/java/com/github/opticyclic/mkparser/MkParser.java
@@ -2,8 +2,19 @@ package com.github.opticyclic.mkparser;
 
 public class MkParser {
 
+    /**
+     * Parse the mk file name from a line.
+     * Assume that all paths start with a variable "$(..)"
+     */
     public String getMkFromLine(String line) {
-        return line;
+        String filename = "";
+        if (line.contains(".mk")) {
+            String trim = line.trim();
+            String substring = trim.substring(trim.indexOf('$'));
+            String[] split = substring.split(" ");
+            filename = split[0];
+        }
+        return filename;
     }
 
 }
